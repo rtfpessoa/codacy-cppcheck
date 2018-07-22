e1f8d25523a948a83688493ea3b60fd5d4595980
Rafael Cortês <rafael@codacy.com>
2018-07-22 10:29:16 +0100

    Use cppcheck from docker container to generate docs

--- scripts/generateDocs.sh
+++ scripts/generateDocs.sh
@@ -7,6 +7,5 @@ current_directory="$( cd "$( dirname "$0" )" && pwd )"
 OUTPUT_FILE="$(mktemp)"
 
 ${current_directory}/publish-base.sh
-docker run -i codacy/codacy-cppcheck-base:latest
-cppcheck --errorlist > $OUTPUT_FILE
+docker run -i codacy/codacy-cppcheck-base:latest --errorlist > $OUTPUT_FILE
 sbt "run-main codacy.cppcheck.DocGenerator $OUTPUT_FILE"
