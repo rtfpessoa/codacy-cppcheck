4d0883b961be357b6510c1d85126356c221fb921
Luís Ferreira <contact@lsferreira.net>
2019-10-10 21:07:20 +0100

    codestyle: run scalafmt
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- src/main/scala/codacy/cppcheck/CPPCheck.scala
+++ src/main/scala/codacy/cppcheck/CPPCheck.scala
@@ -18,30 +18,41 @@ object WarnResult {
 
 object CPPCheck extends Tool {
 
-  lazy val blacklist = Set(
-    "unusedFunction"
-  )
+  lazy val blacklist = Set("unusedFunction")
 
   private val languageConfKey = Options.Key("language")
 
-  override def apply(source: Source.Directory, configuration: Option[List[Pattern.Definition]], files: Option[Set[Source.File]],
-                     options: Map[Options.Key, Options.Value])
-                    (implicit specification: Tool.Specification): Try[List[Result]] = {
+  override def apply(
+      source: Source.Directory,
+      configuration: Option[List[Pattern.Definition]],
+      files: Option[Set[Source.File]],
+      options: Map[Options.Key, Options.Value]
+  )(implicit specification: Tool.Specification): Try[List[Result]] = {
     Try {
       val path = Paths.get(source.path)
       val filesToLint: Seq[String] = files.fold(Seq(source.path)) { paths =>
         paths.map(_.toString).toSeq
       }
 
-      val languageParameter: String = options.get(languageConfKey)
+      val languageParameter: String = options
+        .get(languageConfKey)
         .flatMap { language =>
           Option(language: JsValue).collect {
             case JsString(lang) => s"--language=$lang"
           }
-        }.getOrElse("")
+        }
+        .getOrElse("")
 
-      val command = List("cppcheck", "--enable=all", "--error-exitcode=0", "--inline-suppr", "--force", "-j 2", languageParameter,
-        """--template={"patternId":"{id}","file":"{file}","line":"{line}","message":"{message}"}""") ++
+      val command = List(
+        "cppcheck",
+        "--enable=all",
+        "--error-exitcode=0",
+        "--inline-suppr",
+        "--force",
+        "-j 2",
+        languageParameter,
+        """--template={"patternId":"{id}","file":"{file}","line":"{line}","message":"{message}"}"""
+      ) ++
         filesToLint
 
       CommandRunner.exec(command) match {
@@ -54,13 +65,17 @@ object CPPCheck extends Tool {
     }
   }
 
-  private def parseToolResult(resultFromTool: List[String], path: Path, wasRequested: String => Boolean): List[Result] = {
+  private def parseToolResult(
+      resultFromTool: List[String],
+      path: Path,
+      wasRequested: String => Boolean
+  ): List[Result] = {
     for {
       outputLine <- resultFromTool
       result <- Try(Json.parse(outputLine)).toOption.flatMap(_.asOpt[WarnResult]) if wasRequested(result.patternId)
       line <- Try(result.line.toInt).toOption
-    } yield Issue(Source.File(result.file), Result.Message(result.message),
-      Pattern.Id(result.patternId), Source.Line(line))
+    } yield
+      Issue(Source.File(result.file), Result.Message(result.message), Pattern.Id(result.patternId), Source.Line(line))
   }
 
   private def checkPattern(conf: Option[List[Pattern.Definition]])(patternId: String): Boolean = {
