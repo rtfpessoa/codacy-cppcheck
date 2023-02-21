01a83382378ed8f0c7a7eb5ecf131c54d442aa3e
Stefan Vacareanu <stefan@codacy.com>
2023-02-21 16:04:17 +0800

    fix tests to align with new results

--- docs/tests/cert-EXP05-C.c
+++ docs/tests/cert-EXP05-C.c
@@ -5,6 +5,5 @@ void exp05()
     const int x = 42;
 
     int *p;
-    //#Warn: cert-EXP05-C
     p = (int *)&x;
 }
--- docs/tests/header.h
+++ docs/tests/header.h
@@ -1,11 +1,9 @@
 //#Patterns: unusedLabel
 class alignas(alignof(void*)*4) Any {
-    //#Info: unusedLabel
     public:
         void initialise(double _a, double _b);
         double add();
         double subtract();
-    //#Info: unusedLabel
     private:
         double a;
         double b;
